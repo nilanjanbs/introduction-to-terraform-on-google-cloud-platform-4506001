44926d462d7678f82d0404c6c9d2da01280b8f51
Nilanjan <nilanjanbs@gmail.com>
2025-11-21 16:10:35 -0500

    added network and subnetwork

--- main.tf
+++ main.tf
@@ -1,3 +1,21 @@
+
+resource "google_compute_network" "app" {
+  name                    = "app-network"
+  auto_create_subnetworks = false
+}
+
+
+resource "google_compute_subnetwork" "app" {
+  name          = "app-subnetwork"
+  ip_cidr_range = "10.2.0.0/16"
+  region        = "us-west1"
+  network       = google_compute_network.custom-test.id
+  secondary_ip_range {
+    range_name    = "tf-test-secondary-range-update1"
+    ip_cidr_range = "192.168.10.0/24"
+  }
+}
+
 data "google_compute_image" "ubuntu" {
   most_recent = true
   project     = "ubuntu-os-cloud" 
@@ -15,7 +33,7 @@ resource "google_compute_instance" "web" {
     }
   }
   network_interface {
-   subnetwork = "default"
+   subnetwork = "app"
    access_config {
       # Leave empty for dynamic public IP
     }
