51a9a17c710b6c427c2275cd5032e8783290b674
Nilanjan <nilanjanbs@gmail.com>
2025-11-25 13:08:28 -0500

    deleting non module network

--- main.tf
+++ main.tf
@@ -28,19 +28,6 @@ module "app_network" {
 
 }
 
-resource "google_compute_network" "app" {
-  name                    = var.netowrk_name
-  auto_create_subnetworks = false
-}
-
-resource "google_compute_subnetwork" "app" {
-  name          = "app"
-  ip_cidr_range = "10.2.0.0/16"
-  region        = var.region
-  network       = google_compute_network.app.id
-}
-
-
 data "google_compute_image" "ubuntu" {
   most_recent = true
   project     = var.image-project
