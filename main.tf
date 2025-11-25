eee0c7c248a8324fe35492b48736405efaa940f6
Nilanjan <nilanjanbs@gmail.com>
2025-11-25 17:47:31 -0500

    fixing ip cidr ranges

--- main.tf
+++ main.tf
@@ -2,6 +2,6 @@ module "qa" {
   source = "./modules/blog"
   project_id = var.project_id
   netowrk_name = "qa"
-  ip_range = "10.10.20.0/16"
+  ip_range = "10.10.20.0/24"
   sub_netowrk_name = var.sub_netowrk_name
 }
